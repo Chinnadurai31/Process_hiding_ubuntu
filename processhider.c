e2177550a4e661711e94dc90154ac0cbf185caca
chinnadurai <chinnadurairajagopal7@gmail.com>
2024-05-03 16:20:28 +0530

    update

--- processhider.c
+++ processhider.c
@@ -9,7 +9,7 @@
 /*
  * Every process with this name will be excluded
  */
-static const char* process_to_filter = "chrome";
+static const char* process_to_filter = "<process_name>";
 
 /*
  * Get a directory name given a DIR* handle
